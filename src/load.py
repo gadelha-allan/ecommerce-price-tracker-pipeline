1a3b4643efb60d9f4d237a209e218b51cd2a31c3
gadelha-allan <allanmartinsgadelha@gmail>
2026-07-10 17:46:46 -0300

    refactor: aprimora lógica de carga com tratamento de erros e contexto de conexão

--- src/load.py
+++ src/load.py
@@ -5,16 +5,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def load_to_postgres(df, table_name="prices"):
-
-    DB_USER = os.getenv('DB_USER')
-    DB_PASS = os.getenv('DB_PASS')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT')
-    DB_NAME = os.getenv('DB_NAME')
+    try:
+        DB_USER = os.getenv('DB_USER')
+        DB_PASS = os.getenv('DB_PASS')
+        DB_HOST = os.getenv('DB_HOST')
+        DB_PORT = os.getenv('DB_PORT')
+        DB_NAME = os.getenv('DB_NAME')
     
-    conn_string = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    engine = create_engine(conn_string)
-    
-  
-    df.to_sql(table_name, engine, if_exists='append', index=False)
-    print(f"Dados inseridos com sucesso na tabela {table_name}!")
+        conn_string = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        engine = create_engine(conn_string)
+        
+        with engine.connect() as connection:
+            df.to_sql(table_name, connection, if_exists='append', index=False)
+            print(f"Sucesso: {len(df)} registros inseridos na tabela {table_name}.")
+            
+    except Exception as e:
+        print(f"Erro ao conectar ou inserir dados: {e}")
