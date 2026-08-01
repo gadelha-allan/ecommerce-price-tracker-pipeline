e952b9fc189c43efdea6535db0d4bbfd9adee9ef
Allan Gadelha <allanmartinsgadelha@gmail.com>
2026-08-01 15:26:23 -0300

    refactor: structure logical execution flow

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import logging
-from src.extract import fetch_data
+from src.extract import fetch_data, upload_to_s3
 from src.transform import transform_data
 from src.load import load_to_postgres
-
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def run_pipeline():
     try:
-        logging.info("Iniciando extração de dados...")
-        raw_data = fetch_data("iphone")
+        logging.info("--- Extração e Data Lake ---")
+        raw_data = fetch_data("iphone", max_pages=3)
+        upload_to_s3(raw_data)
         
-        logging.info("Iniciando transformação e limpeza...")
+        logging.info("--- Transformação ---")
         df_cleaned = transform_data(raw_data)
         
-        logging.info("Iniciando carga no PostgreSQL...")
+        logging.info("--- Carga Idempotente ---")
         load_to_postgres(df_cleaned)
         
         logging.info("Pipeline finalizado com sucesso!")
